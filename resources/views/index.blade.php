77d46c98107e3ff0350c7cf6609acffececae456
faisalahammed044274 <setu2014@gmail.com>
2022-07-13 04:55:01 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Management</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Hey</h2>
+    </div>
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>

6fb5be1d74b5846b78a13bab506d72daadc353fe
faisalahammed044274 <setu2014@gmail.com>
2022-07-13 06:42:07 +0600

    Table Show

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     function index(){
-        return view('index');
+
+        $students = Student::all();
+        return view('index')->with('students', $students);
     }
 }
